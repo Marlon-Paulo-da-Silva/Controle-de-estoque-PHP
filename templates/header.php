9261ab20e4c583fe20e1c72cabaff6bc771629db
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-03-14 12:45:09 -0300

    criando a tela de login

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Controle de estoque</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#"><i class="fa fa-home">&nbsp;</i>Inicial</a>
+        <a class="nav-item nav-link active" href="#"><i class="fa fa-user">&nbsp;</i>Logout</a>
+      </div>
+    </div>
+  </nav>
