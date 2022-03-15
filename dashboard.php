0505c31928fad236a9d65539300ade4c19dcf984
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-03-15 17:52:13 -0300

    atualizando card

---
+++ dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Sistema para controle de estoque</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  
+  <script type="text/javascript" src="./js/main.js"></script>
+  <script src=""></script>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <?php include_once("./templates/header.php"); ?>
+
+    <br/><br/>
+
+    <div class="container">
+       <div class="row">
+         <div class="col-md-4">
+         <div class="card mx-auto" style="width: 18rem;">
+            <img class="card-img-top mx-auto" style="width:60%;" src="./images/userid.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"><i class="fa fa-user"></i>Perfil</h5>
+              <p class="card-text"><i class="fa fa-user"></i>Marlon Paulo</p>
+              <p class="card-text">Admin</p>
+              <p class="card-text">Ultimo login</p>
+              <a href="#" class="btn btn-primary"><i class="fa fa-edit">&nbsp;</i>Editar Perfil</a>
+            </div>
+          </div>
+         </div>
+       </div>
+    </div>
+
+    
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
